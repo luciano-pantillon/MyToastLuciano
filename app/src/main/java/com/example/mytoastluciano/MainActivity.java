1248c8e324a1c535ff8e776f82384de338fb99a3
luciano <lucianopantillon@gmail.com>
2019-09-03 11:49:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mytoastluciano/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mytoastluciano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View btn1 = findViewById(R.id.button1)
+        btn1.setOnClickListener( this);
+
+
+
+    }
+    public void onClick(View view)
+    {
+        Toast toast = Toast.makeText(  this,  "Toast Simple", Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+}
